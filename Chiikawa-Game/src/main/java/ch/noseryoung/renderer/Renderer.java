9fb5b4011bd1f696f2506ab33db55fa662ba8ba3
Luka Grujic <luka.grujic@edu.tbz.ch>
2025-07-14 14:49:13 +0200

    small mistake fix

---
+++ Chiikawa-Game/src/main/java/ch/noseryoung/renderer/Renderer.java
@@ -0,0 +1,53 @@
+package ch.noseryoung.renderer;
+
+import ch.noseryoung.chiikawagameengine.GameObject;
+import ch.noseryoung.components.SpriteRenderer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Renderer {
+    private final int MAX_BATCH_SIZE = 1000;
+    private List<RenderBatch> batches;
+
+    public Renderer() {
+        this.batches = new ArrayList<>();
+    }
+
+    public void add(GameObject go) {
+        SpriteRenderer spr = go.getComponent(SpriteRenderer.class);
+        if (spr != null) {
+            add(spr);
+        }
+    }
+
+    private void add(SpriteRenderer sprite) {
+        boolean added = false;
+        for (RenderBatch batch : batches) {
+            if (batch.hasRoom() && batch.getZIndex() == sprite.gameObject.getZIndex()) { // todo: make a render batch exept multiple objects with different zIndexes
+                Texture texture = sprite.getTexture();
+                if (batch.hasTexture(texture) || batch.hasTextureRoom() || texture == null) {
+                    batch.addSprite(sprite);
+                    added = true;
+                    break;
+                }
+            }
+        }
+
+        if (!added) {
+            RenderBatch newBatch = new RenderBatch(MAX_BATCH_SIZE, sprite.gameObject.getZIndex());
+            newBatch.start();
+            batches.add(newBatch);
+            newBatch.addSprite(sprite);
+            Collections.sort(batches);
+        }
+    }
+
+    public void render() {
+        for (RenderBatch batch : batches) {
+            batch.render();
+        }
+    }
+}
